0d500385b34aea733b31ab35ef8f35da8fdad0e9
evyfatela <2312Nabais1988!>
2022-01-21 11:04:47 +0100

    first commit

---
+++ src/main/java/com/timwi/EvelyneAlbumsApp/EvelyneAlbumsAppApplication.java
@@ -0,0 +1,13 @@
+package com.timwi.EvelyneAlbumsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvelyneAlbumsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvelyneAlbumsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timwi/EvelyneAlbumsApp/EvelyneAlbumsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timwi.EvelyneAlbumsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvelyneAlbumsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
